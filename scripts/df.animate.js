75b65221b04edd8b7bb2f0c3ca41468c65b83876
iahmadraza <ahmad.ideveloper@gmail.com>
2016-09-17 21:29:29 +0530

    Add files via upload

---
+++ scripts/df.animate.js
@@ -0,0 +1,40 @@
+//df.animate.js
+(function($){
+	//
+})(jQuery);
+function startPage() {
+	//local storage check
+
+	//1- start
+	//animation end 
+	/*$('div.splash-center-logo').on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
+		console.log('transition done...');
+	});*/
+
+	$('div.splash-center-logo').on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(){
+		console.log('animation done...');
+
+		//go to next page (pagename)
+		//setTimeout($('body').addClass('page-bounceindown'), 3000);
+		//setTimeout($('body').addClass('page-zoomin'), 3000);
+		//setTimeout($('div.splash-center-logo').addClass('el-zoomfade'), 3000);
+		pageTwo();
+		//setTimeout(gotonextpage('landing.html'), 3000);
+	});
+
+	$('div.sp-skyline2').on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(){
+		console.log('animation SKYLINE done...');
+	});
+
+	/*$('div.splash-center-logo').load('dashboard.html', function() {
+		alert('Load was performed.');
+	});*/
+};
+function gotonextpage(pagename) {
+	//console.log('gotonextpage');
+	//window.location.href = pagename;
+};
+function pageTwo() {
+	//
+	$('body').load('landing.html');
+};
